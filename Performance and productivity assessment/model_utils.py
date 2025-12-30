a2123e65582ec86ccfcd387a895f40df73953434
Pavathanya <pavathanyamanoharan@gmail.com>
2025-12-30 14:21:33 +0530

    Small error fixes

---
+++ Performance and productivity assessment/model_utils.py
@@ -0,0 +1,20 @@
+import joblib
+from tensorflow.keras.models import load_model
+
+def load_feedback_model(model_path="Performance Productivity Model/advanced_feedback_model.h5"):
+
+    try:
+        model = load_model(model_path)
+        print(f"Model loaded successfully from {model_path}.")
+        return model
+    except Exception as e:
+        raise FileNotFoundError(f"Failed to load model. Error: {e}")
+
+def load_preprocessor(preprocessor_path="preprocessor.pkl"):
+
+    try:
+        preprocessor = joblib.load(preprocessor_path)
+        print(f"Preprocessor loaded successfully from {preprocessor_path}.")
+        return preprocessor
+    except Exception as e:
+        raise FileNotFoundError(f"Failed to load preprocessor. Error: {e}")
